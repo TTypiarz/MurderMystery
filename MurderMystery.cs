30341497d61cab0b8234fb7a88ace4463f7e0e71
Zereth <70236765+o5zereth@users.noreply.github.com>
2021-09-21 16:35:03 -0400

    Add base classes for the plugin.

---
+++ MurderMystery.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using System;
+
+namespace MurderMystery
+{
+    public class MurderMystery : Plugin<Config>
+    {
+        public override string Author => "Zereth";
+        public override string Name => "MurderMystery";
+        public override string Prefix => "murder_mystery";
+        public override PluginPriority Priority => PluginPriority.Default;
+        public override Version RequiredExiledVersion => new Version(3, 0, 0);
+        public override Version Version => new Version(1, 0, 0);
+
+        public static MurderMystery Singleton { get; private set; }
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Singleton = null;
+
+            base.OnDisabled();
+        }
+    }
+}
