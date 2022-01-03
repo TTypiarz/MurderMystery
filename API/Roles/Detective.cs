aa85800d0c5676886a9de93fc93eb6e676e6a1e4
Zereth <70236765+o5zereth@users.noreply.github.com>
2022-01-03 18:35:32 -0500

    fix a funny typo

--- API/Roles/Detective.cs
+++ API/Roles/Detective.cs
@@ -10,6 +10,6 @@ namespace MurderMystery.API.Roles
 
         public override MMRole Role => MMRole.Detective;
         public override string SpawnMsg => $"<size=70>You are a {ColoredName}</size>";
-        public override string SpawnInfoMsg => "<size=50>You must <color=#ff00ff>protect</color> <color=#ff0000>innocents</color> and <color=#ff00ff>kill</color> <color=#ff0000>murderers</color>.</size>";
+        public override string SpawnInfoMsg => "<size=50>You must <color=#ff00ff>protect</color> <color=#00ff00>innocents</color> and <color=#ff00ff>kill</color> <color=#ff0000>murderers</color>.</size>";
     }
 }
