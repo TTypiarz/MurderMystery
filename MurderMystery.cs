035b483097e6c8401dfb04b0f42e68bb583d58ed
Zereth <70236765+o5zereth@users.noreply.github.com>
2022-05-26 13:24:34 -0400

    Add harmony instance, late round start patch. Add debug check.

--- MurderMystery.cs
+++ MurderMystery.cs
@@ -1,11 +1,18 @@
 ﻿using Exiled.API.Enums;
 using Exiled.API.Features;
+using HarmonyLib;
 using System;
 
 namespace MurderMystery
 {
     public class MurderMystery : Plugin<Config, Translation>
     {
+        static MurderMystery()
+        {
+            Harmony = new Harmony("zereth.plugins.murdermystery");
+            Harmony.PatchAll();
+        }
+
         public override string Author => "Zereth";
         public override string Name => "MurderMystery";
         public override string Prefix => "murder_mystery";
@@ -13,12 +20,26 @@ namespace MurderMystery
         public override Version RequiredExiledVersion => new Version(5, 2, 1);
         public override Version Version => new Version(2, 0, 0);
 
+        public static Harmony Harmony { get; }
+        public static MurderMystery Singleton { get; private set; }
+
+        public const bool InternalDebug = true;
+
         public override void OnEnabled()
         {
+            Singleton = this;
+
+            Config.Validate();
         }
 
         public override void OnDisabled()
         {
+            Singleton = null;
+        }
+
+        public static bool AllowDebug()
+        {
+            return InternalDebug || (Singleton?.Config.Debug ?? false);
         }
     }
 }
---
+++ Patches/LateRoundStartPatch.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.Events.Extensions;
+using HarmonyLib;
+using static Exiled.Events.Events;
+
+namespace MurderMystery.Patches
+{
+    [HarmonyPatch(typeof(CharacterClassManager), nameof(CharacterClassManager.SetRandomRoles))]
+    public static class LateRoundStartPatch
+    {
+        public static event CustomEventHandler LateRoundStarted;
+
+        private static void Postfix()
+        {
+            if (MurderMystery.AllowDebug())
+                Exiled.API.Features.Log.Info("Late round start patch called.");
+
+            LateRoundStarted.InvokeSafely();
+        }
+    }
+}
