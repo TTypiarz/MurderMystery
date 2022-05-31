c2ad0537ca50b3092e65d4485aa879637241c016
Zereth <70236765+o5zereth@users.noreply.github.com>
2022-05-31 13:58:44 -0400

    Assign spawn locations

--- EventHandlers/GamemodeHandlers.cs
+++ EventHandlers/GamemodeHandlers.cs
@@ -1,4 +1,8 @@
-﻿using MurderMystery.API.Internal;
+﻿using Exiled.API.Extensions;
+using Exiled.Events.EventArgs;
+using MurderMystery.API.Enums;
+using MurderMystery.API.Internal;
+using Handlers = Exiled.Events.Handlers;
 
 namespace MurderMystery.EventHandlers
 {
@@ -10,10 +14,37 @@ namespace MurderMystery.EventHandlers
 
         protected override void Enable()
         {
+            Handlers.Player.Spawning += Spawning;
+            Handlers.Player.ChangingRole += ChangingRole;
         }
 
         protected override void Disable()
         {
+            Handlers.Player.Spawning -= Spawning;
+            Handlers.Player.ChangingRole -= ChangingRole;
+        }
+
+        private void Spawning(SpawningEventArgs ev)
+        {
+            if (Plugin.MapPrepared && !Plugin.Started)
+            {
+                ev.Position = Plugin.Zone switch
+                {
+                    MMZone.LCZ => RoleType.Scp173.GetRandomSpawnProperties().Item1,
+                    MMZone.HCZ => RoleType.Scp049.GetRandomSpawnProperties().Item1,
+                    _ => throw new System.InvalidOperationException("Zone has not been set!")
+                };
+            }
+        }
+
+        private void ChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (Plugin.MapPrepared && !Plugin.Started)
+            {
+                ev.NewRole = RoleType.ClassD;
+                ev.Items.Clear();
+                ev.Ammo.Clear();
+            }
         }
     }
 }
